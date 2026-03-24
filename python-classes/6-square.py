f91808f8f8bc797f0d605b9256af5ea331265f43
Your Name <your-email@example.com>
2026-03-24 11:45:18 -0700

    Add task 6: Add position attribute and coordinate printing to Square

---
+++ python-classes/6-square.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""Module that defines a Square with size and position."""
+
+
+class Square:
+    """Defines a square by its size and position."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes the square instance.
+
+        Args:
+            size (int): The size of the square.
+            position (tuple): The coordinates of the square.
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Retrieves the size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size with validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Retrieves the position."""
+        return self.__position
+
+    @size.setter
+    @position.setter
+    def position(self, value):
+        """Sets the position with validation."""
+        if (not isinstance(value, tuple) or len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints the square with # considering its position."""
+        if self.__size == 0:
+            print("")
+            return
+
+        [print("") for i in range(self.__position[1])]
+        for i in range(self.__size):
+            print("{}{}".format(" " * self.__position[0], "#" * self.__size))
